4bb4cbb12220f4dc504998fc293a854d86967e64
federico CV <fedesosa2002@hotmail.com>
2023-01-27 18:08:10 -0300

    CV-curso

---
+++ fuctionCV.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("boton_color").addEventListener("click", function () {
+    document.body.style.backgroundColor= "#A0522D";
+     
+ });
+
+
+ document.getElementById("boton_default").addEventListener("click", function () {
+    document.body.style.backgroundColor= "darkslategray";
+     
+ });
